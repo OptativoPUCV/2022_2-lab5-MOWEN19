3cd29be4aade4269b2f169ffff6a121985b0fa20
Replit user <>
2022-09-26 21:27:45 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -46,7 +46,7 @@ TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
 
 
 void insertTreeMap(TreeMap * tree, void* key, void * value) {
-  TreeNode *new = createTreeNode(key,value);
+    TreeNode *new = createTreeNode(key,value);
 
   // si es vacio el arbol
   
@@ -75,10 +75,9 @@ void insertTreeMap(TreeMap * tree, void* key, void * value) {
       auxRecorrido = auxRecorrido->right;
     }
     if(tree->lower_than(key,auxRecorrido->pair->key)){
-      
+      puts("izq\n");
       if(auxRecorrido->left == NULL){
         //TreeNode *new = createTreeNode(key,value);
-        puts("izq\n");
         auxRecorrido->left = new;
         tree->current->left = auxRecorrido;
         return ;
