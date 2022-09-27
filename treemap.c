b5a0f975fa2da477a877c1a360cd5e4f48fec8e8
Replit user <>
2022-09-27 03:11:15 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -116,7 +116,7 @@ void removeNode(TreeMap * tree, TreeNode* node) {
           auxRecorrido->parent->right = NULL;
           return;
         }
-        else if(auxRecorrido->left != NULL){
+        else if(auxRecorrido->left != NULL && auxRecorrido->right == NULL){
           auxRecorrido->left->parent = auxRecorrido->parent;
           puts("n\n");
           return;
